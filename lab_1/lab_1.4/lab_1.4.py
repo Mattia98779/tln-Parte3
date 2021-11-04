291c69bc0a90a6a9d018cf86bf94f6d01b08b32b
mattia zito <mattia.zito@edu.unito.it>
2021-11-04 18:01:32 +0100

    lab_1.4

--- lab_1/lab_1.4/lab_1.4.py
+++ lab_1/lab_1.4/lab_1.4.py
@@ -1,16 +1,49 @@
 from nltk.corpus import brown
 import spacy
+from nltk.wsd import lesk
+from spacy import displacy
+from spacy.symbols import nsubj, VERB
+
 nlp = spacy.load('en_core_web_sm')
-a = ["give", "gives", "gave", "given", "giving"]
+transitive_verb =['make','made','making','makes']
 l = brown.sents()
 sentens = []
 for s in l:
     if s != None:
-        if any(verb in s for verb in a):
-            sentens.append(s)
-
-doc = nlp(str(sentens[0]))
-for sent in doc:
-    for token in sent:
-        if token.is_alpha:
-            print (token.orth_, token.tag_, token.head.lemma_)
+        if any(verb in s for verb in transitive_verb):
+            sentences.append(s)
+print("n frasi= ", len(sentences))
+tot = 0
+# frase, soggetto, verbo, oggetto
+frasi = []
+for sentence in sentences:
+    test = ""
+    for word in sentence:
+        test += " " + word
+    #print(test)
+    doc = nlp(test)
+    trovato = 0
+    soggetto = None
+    oggetto = None
+    verbo = None
+    for token in doc:
+        if token.text in transitive_verb:
+            verbo = token
+            #print("verbo = ",token.text, token.dep_)
+            for child in token.children:
+                if child.dep_=="dobj":
+                    soggetto = child
+                    #print("complemento oggetto = ", child.text, child.dep_)
+                    trovato +=1
+                if child.dep_ == "nsubj" or child.dep_=="nsubjpass":
+                    oggetto = child
+                    #print("soggetto = ", child.text, child.dep_)
+                    trovato +=1
+    if trovato==2:
+        tot+=1
+        print(tot)
+        frasi.append([sentence,soggetto,verbo,oggetto])
+print(frasi[0])
+senso = lesk(frasi[0][0], frasi[0][1].text)
+print(senso)
+print(senso.lexname())
